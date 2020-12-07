a5bcece176a02ec74fa8d57f07c359101ad497e2
WWL <819070864@qq.com>
2020-12-07 23:51:51 +0800

    Add project files.

---
+++ IpReflection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ipMicroService
+{
+    public class IpReflection:IIpReflection
+    {
+        private IHttpContextAccessor _httpContextAccessor;
+        public IpReflection(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetIp()
+        {
+            var headerPairs = _httpContextAccessor.HttpContext.Request.Headers;
+            string XForIP = headerPairs["X-Forwarded-For"];
+            if (XForIP == null)
+                XForIP = "Null";
+            return XForIP;
+        }
+    }
+}
